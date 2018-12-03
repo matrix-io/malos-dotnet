c34cdc46e718f1b0ce2f6021cf3e0bf0e90f252e
leonardo <leonardo.vernaza@admobilize.com>
2018-12-03 15:59:25 -0500

    First implementation of Malos library for .Net

---
+++ matrix-io-malos/example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            matrix_io_malos.Driver driver = new matrix_io_malos.Driver("127.0.0.1", matrix_io_malos.BasePort.Vision);
+            driver.onGetData += Driver_onGetData;
+            driver.getData();
+        }
+
+        private static void Driver_onGetData(byte[] data)
+        {
+            MatrixIO.Vision.V1.VisionResult result;
+            result = MatrixIO.Vision.V1.VisionResult.Parser.ParseFrom(data);
+        }
+    }
+}
--- matrix-io-malos/matrix-io-malos/Driver.cs
+++ matrix-io-malos/matrix-io-malos/Driver.cs
@@ -1,12 +1,193 @@
-﻿using System;
+﻿using Google.Protobuf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using ZeroMQ;
 
 namespace matrix_io_malos
 {
+    public delegate void GetData(byte[] data);
+    public delegate void GetStatus(MatrixIO.Malos.Driver.V1.Status status);
+
+    public enum BasePort { Imu = 1, Humidity, Everloop, Pressure, UV, Mic, Vision };
+
     public class Driver
     {
+        /// <summary>
+        /// Event to receive the data from MALOS.
+        /// </summary>
+        public event GetData onGetData;
+
+        /// <summary>
+        /// Event to receive the satus message.
+        /// </summary>
+        public event GetStatus onGetStatus;
+
+        private const int imuPort = 20013;
+        private const int humidityPort = 20017;
+        private const int everloopPort = 20021;
+        private const int pressurePort = 20025;
+        private const int uvPort = 20029;
+        private const int micArrayPort = 20037;
+        private const int visionPort = 60001;
+
+        private string address;
+        private int basePort;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="address">IP address of the device exposing the MALOS 0MQ sockets</param>
+        /// <param name="basePort">MALOS base port to use</param>
+        public Driver(string address, BasePort basePort)
+        {
+            this.address = address;
+            switch (basePort)
+            {
+                case BasePort.Imu:
+                    this.basePort = imuPort;
+                    break;
+                case BasePort.Humidity:
+                    this.basePort = humidityPort;
+                    break;
+                case BasePort.Everloop:
+                    this.basePort = everloopPort;
+                    break;
+                case BasePort.Pressure:
+                    this.basePort = pressurePort;
+                    break;
+                case BasePort.UV:
+                    this.basePort = uvPort;
+                    break;
+                case BasePort.Mic:
+                    this.basePort = micArrayPort;
+                    break;
+                case BasePort.Vision:
+                    this.basePort = visionPort;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// It sends the provided configuration proto, the driver.proto, to the Malos
+        /// Configuration prot to configure the driver.
+        /// </summary>
+        /// <param name="configProto">a driver.proto containing configuration for the driver</param>
+        /// <param name="timeOut">timeout in seconds to deliver configuration data to Malos</param>
+        public void configure(MatrixIO.Malos.Driver.V1.DriverConfig configProto, int timeOut)
+        {
+            using (var context = new ZContext())
+            using (var socket = new ZSocket(context, ZSocketType.PUSH))
+            {
+                socket.Connect(String.Format("tcp://{0}:{1}", address, basePort));
+                byte[] bytes = configProto.ToByteArray();
+                using (var configFrame = new ZFrame(bytes))
+                {
+                    socket.Send(configFrame);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Connects to the corresponding keep-alive port (basePort + 1) given the
+        /// desired basePort. We send keep alive pings and receive pongs so MALOS
+        /// keeps sampling the data from the sensors and we can keep yielding with gedData()
+        /// </summary>
+        /// <param name="delay">delay between pings in seconds</param>
+        /// <param name="timeout">how long to wait for pongs before timeout in seconds</param>
+        public void startKeepAlive(int delay = 5, int timeout = 5)
+        {
+            using (var context = new ZContext())
+            using (var socket = new ZSocket(context, ZSocketType.REQ))
+            {
+                socket.Connect(String.Format("tcp://{0}:{1}", address, (basePort + 1)));
+                socket.SetOption(ZSocketOption.RCVTIMEO, (timeout * 1000));
+                while (true)
+                {
+                    socket.Send(new ZFrame(""));
+                    using (ZFrame reply = socket.ReceiveFrame())
+                    {
+                        Console.WriteLine(reply.ReadString());
+                        Thread.Sleep(delay * 1000);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Connects to the corresponding status port (base_port +2) given the desired
+        /// basePort and raise an onGetSatus event.
+        /// </summary>
+        public void getStatus()
+        {
+            using (var context = new ZContext())
+            using (var socket = new ZSocket(context, ZSocketType.SUB))
+            {
+                socket.Connect(String.Format("tcp://{0}:{1}", address, basePort + 2));
+                socket.Subscribe("");
+                while (true)
+                {
+                    try
+                    {
+                        while (true)
+                        {
+                            using (ZFrame data = socket.ReceiveFrame())
+                            {
+                                byte[] bytes = data.Read();
+                                MatrixIO.Malos.Driver.V1.Status status;
+                                status = MatrixIO.Malos.Driver.V1.Status.Parser.ParseFrom(bytes);
+                                onGetStatus?.Invoke(status);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("status-port: error " + ex.Message);
+                    }
+                    finally
+                    {
+                        Console.WriteLine("status-port: socket closed");
+                        socket.Close();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Connects to the corresponding data port (base_port +3) and raise
+        /// an onGetData event with the messages received through it.
+        /// </summary>
+        public void getData()
+        {
+            using (var context = new ZContext())
+            using (var socket = new ZSocket(context, ZSocketType.SUB))
+            {
+                socket.Connect(String.Format("tcp://{0}:{1}", address, basePort + 3));
+                socket.Subscribe("");
+                try
+                {
+                    while (true)
+                    {
+                        using (ZFrame data = socket.ReceiveFrame())
+                        {
+                            byte[] bytes = data.Read();
+                            onGetData?.Invoke(bytes);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("data-port: error " + ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("data-port: socket closed");
+                    socket.Close();
+                }
+            }
+        }
     }
 }
